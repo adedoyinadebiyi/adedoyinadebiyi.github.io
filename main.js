46011fa4dc1d726407a979559506331b19915fa7
adedoyinadebiyi <adedoyinadebiyi@gemschicago.org>
2022-02-23 12:17:58 -0600

    button

---
+++ main.js
@@ -0,0 +1,177 @@
+
+/*
+
+var dayofWeek = prompt ("What is today?");
+
+
+if (dayofWeek == "Monday"){
+	alert("Go to Design");
+}
+
+else if (dayofWeek == "Tuesday") {
+	alert ("Go to Lakeshore");
+}
+
+else{
+	alert ("Go home!");
+}
+
+
+
+
+var student = "Adedoyin";
+var age = 15;
+var isRnrolled = false;
+var testscore =95;
+var testscore2 = 90;
+var averagescore = (testscore + testscore2)/2;
+console.log (" Test average: " + averagescore);
+console.log ("student:" + student);
+
+var roster = ["Bown", "Ariel", "Noah K", "Diego",]
+
+student = roster[0];
+console.log ("student:" + student);
+
+roster.push("Varun");
+console.log(roster);
+
+
+var giftCard = 100;
+
+while (giftCard >=0) {
+	console.log("You have a balance of: $" + giftCard);
+giftCard = giftCard - 10;
+if (giftCard ==50 ) { 
+     console.log("You have $50 left. You should consider saving!");
+     }
+}
+
+consol.log ("You are out of money!");
+
+
+console.log ("You are out of money");
+
+for (var giftCard = 100; giftCard>=0; giftCard--) {
+	console.log(giftCard):
+	if (giftCard ==50){
+
+	}
+}
+ 
+var = modelS = {
+	make: "Tesla",
+	model: "Model S",
+	cost: 70000,
+	preowened: false,
+	miles: o,
+	finalCost:function(discount) {
+		this.cost = this.cost - (this.cost * (discount/100));
+		return this.cost;
+	}
+};
+
+var updatedMiles= modelS.miles + 70000;
+console.log(modelS.make);
+console.log (modelS.model);
+
+console.log(modelS.finalCost(30));
+console.log (updatedMiles);
+
+console.log(modelS.make.toUpperCase());
+
+
+function Hotel(hotelName,rooms, bokked){
+	this.name = hotelName;
+	this.rooms = rooms;
+	this.booked = booked;
+	this,checkAvailability = function() {
+		return this.rooms - this.booked;
+	};
+}
+
+var disneyHotel = new Hotel ('Animal Kingdom Lodge', 100, 25)
+
+
+console.log (disneyHotel.name);
+console.log ("Numbers of rooms booked:" + disneyHotel.booked)
+console.log (disneyHotel.checkAvailability())
+
+
+
+function Phonecase(name,model,size,price,){
+	this.name = phonecaseName;
+	this.size= size;
+	this.soldout = soldout
+	this.price= price;
+	this.model = model;
+	this,checkifinStock = function(){
+	     return this.model - this.soldout; 
+	 if quantity is soldout = noinstock
+
+	};
+}
+
+if q is =nto zero 
+
+
+var= Wildflowercases 
+
+*/
+
+#intro{
+	background-color: blue;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
