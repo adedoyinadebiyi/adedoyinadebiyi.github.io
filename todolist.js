dc828c84163306691fbf296586997bbfc0bc3ac8
adedoyinadebiyi <adedoyinadebiyi@gemschicago.org>
2022-04-07 08:51:23 -0500

    To do list code.

---
+++ todolist.js
@@ -0,0 +1,15 @@
+window,onload = function() {
+  //var greating;
+  //var element = document.getElementById("intro");
+
+  var button = document.querySelector("button");
+  button.addEventListener("click", addText);
+
+
+  function addText() {
+    var textFeild = document.getElementById("textfield");
+    var msg = "<h1> HOT PINK!</h1>";
+    textFeild.innerHTML = msg;
+  }
+
+}
